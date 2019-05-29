fefeee4cc406801badcf55e1f2fc3410a806ec46
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-05-29 18:29:45 -0400

    Fix statuses, so that updates will work

--- app/controllers/api/v1/statuses_controller.rb
+++ app/controllers/api/v1/statuses_controller.rb
@@ -6,10 +6,15 @@ class Api::V1::StatusesController < ApplicationController
   def add_new
     @meetup_group = Group.find_by_meetup_group_id(params[:groupId])
     @status = Status.from_json(params[:status])
-    @new_status = UserGroupStatus.where(
+    @find_status = UserGroupStatus.find_or_create_by(
         user_id: @current_user.id,
-        group_id: @meetup_group.id,
-        status_id: @status.id).first_or_create
+        group_id: @meetup_group.id
+        )
+    @find_status.update_attributes(
+      status_id: @status.id,
+      user_id: @current_user.id,
+      group_id: @meetup_group.id
+    )
 
     render json: @status
     # binding.pry
@@ -23,7 +28,6 @@ class Api::V1::StatusesController < ApplicationController
       group_id: @meetup_group.id,
       user_id: @user.id
     )
-    # binding.pry
     @status = Status.find_by_id(@user_group_status.status_id)
 
     render json: @status
